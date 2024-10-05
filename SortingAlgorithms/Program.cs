430672e139b11e412edf8626d278594bf3c5d976
RANDRIANANTENAINA Nambinintsoa Mickaël <mickael.nambinintsoa07081999@gmail.com>
2024-10-05 10:38:40 +0400

    Implement Merge sort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -54,6 +54,64 @@
         }
     }
 
+    // Tri fusion (Merge Sort).
+    public static void MergeSort(int[] arr, int l, int r)
+    {
+        if (l < r)
+        {
+            int m = l + (r - l) / 2;
+            MergeSort(arr, l, m);
+            MergeSort(arr, m + 1, r);
+            Merge(arr, l, m, r);
+        }
+    }
+
+    private static void Merge(int[] arr, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
+
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        i = 0;
+        j = 0;
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
     public static void Main(string[] args)
     {
         int[] arr1 = { 64, 34, 25, 12, 22, 11, 90 };
@@ -73,6 +131,12 @@
         PrintArray(arr3);
         InsertionSort(arr3);
         PrintArray(arr3);
+
+        int[] arr5 = { 12, 11, 13, 5, 6, 7 };
+        Console.WriteLine("\nTri fusion:");
+        PrintArray(arr5);
+        MergeSort(arr5, 0, arr5.Length - 1);
+        PrintArray(arr5);
     }
 
     private static void PrintArray(int[] arr)
