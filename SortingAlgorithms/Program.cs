e857cbf99212e9e426103132e3c2dd1300ec86ab
RANDRIANANTENAINA Nambinintsoa Mickaël <mickael.nambinintsoa07081999@gmail.com>
2024-10-05 10:41:33 +0400

    Implement Heap sort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -143,6 +143,46 @@
         return i + 1;
     }
 
+    // Tri par tas (Heap Sort).
+    public static void HeapSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(arr, n, i);
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            Heapify(arr, i, 0);
+        }
+    }
+
+    private static void Heapify(int[] arr, int n, int i)
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            Heapify(arr, n, largest);
+        }
+    }
+
     public static void Main(string[] args)
     {
         int[] arr1 = { 64, 34, 25, 12, 22, 11, 90 };
@@ -174,6 +214,12 @@
         PrintArray(arr4);
         QuickSort(arr4, 0, arr4.Length - 1);
         PrintArray(arr4);
+
+        int[] arr6 = { 12, 11, 13, 5, 6, 7 };
+        Console.WriteLine("\nTri par tas:");
+        PrintArray(arr6);
+        HeapSort(arr6);
+        PrintArray(arr6);
     }
 
     private static void PrintArray(int[] arr)
